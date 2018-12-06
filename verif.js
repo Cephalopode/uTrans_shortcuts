4e0f99e4c9741ffec6f2254dc5c27fe1d712da06
thomas <thomas@MacBook-Pro-2.local>
2018-12-06 15:17:07 +0800

    added excels

--- verif.js
+++ verif.js
@@ -1,49 +1,77 @@
 
 tbod = '.tbody > tr'
 total = 0
+ren = ""
 for (let i = 0; i < $(tbod).length; i++) {
 	//console.log($(tbod+':eq('+i+') td:eq(5)').text)
 	if(!!$(tbod+':eq('+i+') td:eq(4)')[0].innerText.match('OK\\(100\\)')) {
-		str=$(tbod+':eq('+i+') td:eq(5)')[0].innerText.match('Valid: (.+?) Millisecond')[1]
-		total += parseInt(str)
-		console.log(i + ": " + str)
+		id = parseInt($(tbod+':eq('+i+') td:eq(0)')[0].innerText)
+		if(id >= 0 && id <= 11116) {
+			str=$(tbod+':eq('+i+') td:eq(5)')[0].innerText.match('Valid: (.+?) Millisecond')[1]
+			total += parseInt(str)
+			//console.log(i + ": " + str)
+			renwuId = $(tbod+':eq('+i+') td:eq(0)')[0].innerText
+			ren += ("\n" + renwuId)
+		}
 	}
 }
+console.log(ren)
 console.log('total: '+total/1000)
 
 
 pages =
-`2341.434
-2169.614
-2538.547
-1982.542
-2560.732
-2061.174
-962.052`
-pages.replace(/\n/g,"+")
+`509.027
+1035.981
+1725.669
+3050.83
+792.017`
+eval(pages.replace(/\n/g,"+"))/3600*700
+
+/* 
+13715.791
+*/
+
 /*
-213.78933333333333 +              
-525.9368888888889 +
-512.99 +
-499.069 +
-507.116 +
-536.147 +
-452.97
+ pasge 8
+1913.565 2341.434
+489.617 962.052
+page 2
 
 /* 10-31 => 11-27
  2eme essai 
  pasge 8
 
-2341.434
+1913.565 
 2169.614
 2538.547
 1982.542
 2560.732
 2061.174
-962.052
+489.617 
 
 page 2
 */
 
 
 /*303056+360028+308536+367078+181163+188637+205067+180664+247205 = 2341434
+
+4616.095 / 3600 * 800 = 3248.02
+========================
+
+debut => 11116
+509.027
+1035.981
+1725.669
+3050.83
+792.017
+
+= 1383.1852222222221 元   (mais elle m'a donne juste 1059)
+=======================
+
+debut => 11116
+1059 元
+
+11118 => 17399
+3047.9535 元 （mais elle m'a donne 3248)
+
+
